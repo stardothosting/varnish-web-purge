143921bf6632a637eb648349962b03ea813ac311
Kevin Kutzko <k@kevinkevin.com>
2013-08-27 16:24:18 -0400

    added license to main php script

--- purge.php
+++ purge.php
@@ -1,4 +1,20 @@
 <?php
+# Managed Hosting Services : www.stardothosting.com
+# Web Design Toronto : www.shift8web.com
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program in the LICENSE file. If not, see <http://www.gnu.org/licenses/>
+
 header( 'Content-Type: text/plain' );
 header( 'Cache-Control: max-age=0' );
 
